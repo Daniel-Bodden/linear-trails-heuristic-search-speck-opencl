9213522af5aeed82fcd040afee7a832c26ec7931
db <daniel.bodden@gmail.com>
2020-03-05 08:14:16 +0100

    added 48bit

---
+++ approach1/48bit/heuristic.h
@@ -0,0 +1,37 @@
+#ifndef __LSARXCL__heuristic__
+#define __LSARXCL__heuristic__
+
+#include <stdio.h>
+
+
+//Include source files for OPENCL
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+namespace heuristic_lib
+{
+    class Heuristic
+    {
+    public:
+        Heuristic();                                    //contructor
+        void generateKernel();
+        void displayInfo();
+        void runCipher();
+        void checkErr(cl_int err, const char * name);
+        void printBinary(cl_long n, cl_long blocksize);
+        void saveRoundResultsComboToFile(cl_ulong *arrayResults, const char* filename, cl_ulong nrRounds, int typeOfoutput);
+        double get_wall_time();
+        double get_cpu_time();
+        virtual ~Heuristic();                                                 // Destructor
+        
+        private:
+        
+       
+    };// END CLASS
+    
+};//END NAMESPACE
+
+#endif /* defined(__LSARXCL__computation__) */
